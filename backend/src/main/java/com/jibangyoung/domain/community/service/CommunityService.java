3f5a27e81501f90fc9e8fea0ab4a28a4e673dcc6
yerok67 <yerock81@gmail.com>
2025-07-16 17:38:23 +0900

    커뮤니티 메인

---
+++ backend/src/main/java/com/jibangyoung/domain/community/service/CommunityService.java
@@ -0,0 +1,29 @@
+package com.jibangyoung.domain.community.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.jibangyoung.domain.community.dto.PostListDto;
+import com.jibangyoung.domain.community.repository.PostRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CommunityService {
+    private final PostRepository postRepository;
+
+    // public List<PostListDto> getRecentTop10(LocalDateTime since) {
+    //     return postRepository.findTop10ByCreatedAtAfterOrderByLikeDesc(since).stream()
+    //             .map(post -> new PostListDto(
+    //                     post.getTitle(),
+    //                     post.getLikes(),
+    //                     post.getViews(),
+    //                     post.getCreatedAt(),
+    //                     post.getRegionId()))
+    //             .collect(Collectors.toList());
+    // }
+}
