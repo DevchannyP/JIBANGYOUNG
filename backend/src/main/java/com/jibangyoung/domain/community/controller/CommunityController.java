495343073ea9d3862fb5729ae3a3609682e205ba
yerok67 <yerock81@gmail.com>
2025-07-21 10:20:04 +0900

    인기글 TOP 10

--- backend/src/main/java/com/jibangyoung/domain/community/controller/CommunityController.java
+++ backend/src/main/java/com/jibangyoung/domain/community/controller/CommunityController.java
@@ -2,6 +2,7 @@ package com.jibangyoung.domain.community.controller;
 
 import java.util.List;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,14 +15,13 @@ import lombok.NoArgsConstructor;
 
 @RestController
 @RequestMapping("/api/community")
-@NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CommunityController {
 
     CommunityService communityService;
 
-    // @GetMapping("/top-liked-week")
-    // public List<PostListDto> topLikeToday() {
-    //     return communityService.getRecentTop10(null);
-    // }
+     @GetMapping("/top-liked-week")
+     public List<PostListDto> topLikeToday() {
+         return communityService.getRecentTop10(null);
+     }
 }
