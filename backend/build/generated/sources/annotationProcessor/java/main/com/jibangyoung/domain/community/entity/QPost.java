39dd24f79d9c3315b4c34b95055ab5acf9829566
yerok67 <yerock81@gmail.com>
2025-07-23 11:25:50 +0900

    build 수정

---
+++ backend/build/generated/sources/annotationProcessor/java/main/com/jibangyoung/domain/community/entity/QPost.java
@@ -0,0 +1,63 @@
+package com.jibangyoung.domain.community.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QPost is a Querydsl query type for Post
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QPost extends EntityPathBase<Post> {
+
+    private static final long serialVersionUID = 1298507734L;
+
+    public static final QPost post = new QPost("post");
+
+    public final EnumPath<Post.PostCategory> category = createEnum("category", Post.PostCategory.class);
+
+    public final StringPath content = createString("content");
+
+    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath isDeleted = createBoolean("isDeleted");
+
+    public final BooleanPath isMentorOnly = createBoolean("isMentorOnly");
+
+    public final BooleanPath isNotice = createBoolean("isNotice");
+
+    public final NumberPath<Integer> likes = createNumber("likes", Integer.class);
+
+    public final NumberPath<Long> regionId = createNumber("regionId", Long.class);
+
+    public final StringPath tag = createString("tag");
+
+    public final StringPath title = createString("title");
+
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = createDateTime("updatedAt", java.time.LocalDateTime.class);
+
+    public final NumberPath<Long> userId = createNumber("userId", Long.class);
+
+    public final NumberPath<Integer> views = createNumber("views", Integer.class);
+
+    public QPost(String variable) {
+        super(Post.class, forVariable(variable));
+    }
+
+    public QPost(Path<? extends Post> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QPost(PathMetadata metadata) {
+        super(Post.class, metadata);
+    }
+
+}
+
