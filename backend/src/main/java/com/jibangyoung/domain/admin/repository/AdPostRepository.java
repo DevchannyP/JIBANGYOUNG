62f1a62b4234c35f5678be91725373eb14268688
ParkChanHUI <condingobsession09@gmail.com>
2025-07-26 02:29:25 +0900

    동인까지

--- backend/src/main/java/com/jibangyoung/domain/admin/repository/AdPostRepository.java
+++ backend/src/main/java/com/jibangyoung/domain/admin/repository/AdPostRepository.java
@@ -3,9 +3,10 @@ package com.jibangyoung.domain.admin.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.jibangyoung.domain.community.entity.Post;
 
-public interface AdPostRepository extends JpaRepository<Post, Long>{
+import com.jibangyoung.domain.community.entity.Posts;
+
+public interface AdPostRepository extends JpaRepository<Posts, Long> {
     // 게시글관리(최신순 정렬)
-    List<Post> findAllByOrderByCreatedAtDesc();
-} 
+    List<Posts> findAllByOrderByCreatedAtDesc();
+}
--- backend/src/main/java/com/jibangyoung/domain/admin/service/AdPostService.java
+++ backend/src/main/java/com/jibangyoung/domain/admin/service/AdPostService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jibangyoung.domain.admin.dto.AdPostDTO;
 import com.jibangyoung.domain.admin.repository.AdPostRepository;
-import com.jibangyoung.domain.community.entity.Post;
+import com.jibangyoung.domain.community.entity.Posts;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,28 +15,29 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AdPostService {
     private final AdPostRepository adPostRepository;
+
     // 게시글 관리_조회
     public List<AdPostDTO> getAllPosts() {
         return adPostRepository.findAllByOrderByCreatedAtDesc()
-            .stream()
-            .map(this::toDTO)
-            .collect(Collectors.toList());
+                .stream()
+                .map(this::toDTO)
+                .collect(Collectors.toList());
     }
 
     // 게시글 관리_삭제
-    public void deletePost(Long id){
+    public void deletePost(Long id) {
         adPostRepository.deleteById(id);
     }
+
     // DTO 생성
-    private AdPostDTO toDTO(Post post) {
+    private AdPostDTO toDTO(Posts post) {
         return new AdPostDTO(
                 post.getId(),
                 post.getTitle(),
                 post.getUserId(),
-                post.getCreatedAt().toLocalDate(), 
+                post.getCreatedAt().toLocalDate(),
                 post.getRegionId(),
                 post.getViews(),
-                post.getLikes()
-        );
+                post.getLikes());
     }
 }
