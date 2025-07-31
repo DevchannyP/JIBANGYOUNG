393b0fc72210fabf28245d6be3f08799449dc881
yerok67 <yerock81@gmail.com>
2025-07-31 15:05:01 +0900

    글 작성 수정
    테스트 파일 수정
    Suspense 감싸기

--- frontend/app/community/write/page.tsx
+++ frontend/app/community/write/page.tsx
@@ -1,5 +1,10 @@
-import WriteForm from "./writeForm";
+import { Suspense } from "react";
+import WriteForm from "./WriteForm";
 
-export default function HelloPage() {
-  return <WriteForm />;
+export default function Page() {
+  return (
+    <Suspense fallback={<div>로딩 중...</div>}>
+      <WriteForm />
+    </Suspense>
+  );
 }
