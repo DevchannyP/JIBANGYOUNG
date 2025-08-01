9dc508e9e351845b9c385b218fd3bec57885b71b
WONDongin <88768560+WONDongin@users.noreply.github.com>
2025-08-01 17:02:37 +0900

    멘토 활동로그 리스트  완료

--- backend/src/main/java/com/jibangyoung/domain/mentor/dto/AdMentorLogListDTO.java
+++ backend/src/main/java/com/jibangyoung/domain/mentor/dto/AdMentorLogListDTO.java
@@ -1,5 +1,7 @@
 package com.jibangyoung.domain.mentor.dto;
 
+import com.jibangyoung.domain.auth.entity.UserRole;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,13 +19,13 @@ public class AdMentorLogListDTO {
     private Long reportProcessed;   
 
     // JPQL에서 enum 타입 매핑을 위한 생성자
-    // public AdMentorLogListDTO(Long id, String nickname, UserRole role, Long regionId, Long postCount, Long commentCount, Long reportProcessed) {
-    //     this.id = id;
-    //     this.nickname = nickname;
-    //     this.role = (role != null) ? role.name() : null;
-    //     this.regionId = regionId;
-    //     this.postCount = postCount;
-    //     this.commentCount = commentCount;
-    //     this.reportProcessed = reportProcessed;
-    // }
+    public AdMentorLogListDTO(Long id, String nickname, UserRole role, Long regionId, Long postCount, Long commentCount, Long reportProcessed) {
+        this.id = id;
+        this.nickname = nickname;
+        this.role = (role != null) ? role.name() : null;
+        this.regionId = regionId;
+        this.postCount = postCount;
+        this.commentCount = commentCount;
+        this.reportProcessed = reportProcessed;
+    }
 }
