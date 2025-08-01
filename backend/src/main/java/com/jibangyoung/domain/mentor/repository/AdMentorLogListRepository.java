317eb6bf6f479022183e0f3ad7002283428fcaae
WONDongin <88768560+WONDongin@users.noreply.github.com>
2025-08-01 17:02:37 +0900

    멘토 활동로그 리스트  완료

--- backend/src/main/java/com/jibangyoung/domain/mentor/repository/AdMentorLogListRepository.java
+++ backend/src/main/java/com/jibangyoung/domain/mentor/repository/AdMentorLogListRepository.java
@@ -1,25 +1,35 @@
 package com.jibangyoung.domain.mentor.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.jibangyoung.domain.auth.entity.User;
+import com.jibangyoung.domain.mentor.dto.AdMentorLogListDTO;
+import com.jibangyoung.domain.mypage.entity.ReviewResultCode;
 
 public interface AdMentorLogListRepository extends JpaRepository<User, Long> {
 
-// @Query("""
-//     SELECT new com.jibangyoung.domain.mentor.dto.AdMentorLogListDTO(
-//         u.id,
-//         u.nickname,
-//         u.role,
-//         m.regionId,
-//         (SELECT COUNT(p) FROM Posts p WHERE p.userId = u.id),
-//         (SELECT COUNT(c) FROM Comment c WHERE c.user.id = u.id),
-//         (SELECT COUNT(r) FROM Report r WHERE r.reviewedBy = u.id AND r.reviewResultCode = com.jibangyoung.domain.report.entity.ReportStatus.APPROVED)
-//     )
-//     FROM MentorTest m
-//     JOIN User u ON m.userId = u.id
-//     WHERE m.regionId IN :regionIds
-// """)
-// List<AdMentorLogListDTO> findMentorLogListByRegionIds(@Param("regionIds") List<Long> regionIds);
+@Query("""
+    SELECT new com.jibangyoung.domain.mentor.dto.AdMentorLogListDTO(
+        u.id,
+        u.nickname,
+        u.role,
+        m.regionId,
+        (SELECT COUNT(p) FROM Posts p WHERE p.userId = u.id),
+        (SELECT COUNT(c) FROM Comment c WHERE c.user.id = u.id),
+        (SELECT COUNT(r) FROM Report r WHERE r.reviewedBy = u.id AND r.reviewResultCode = :approved)
+    )
+    FROM MentorTest m
+    JOIN User u ON m.userId = u.id
+    WHERE m.regionId IN :regionIds
+""")
+List<AdMentorLogListDTO> findMentorLogListByRegionIds(
+    @Param("regionIds") List<Long> regionIds,
+    @Param("approved") ReviewResultCode approved
+);
+
 
 }
