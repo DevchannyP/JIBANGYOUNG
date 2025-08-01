e4c13daa31757b98dd6d164270985c1eb64b9e03
WONDongin <88768560+WONDongin@users.noreply.github.com>
2025-08-01 17:02:37 +0900

    멘토 활동로그 리스트  완료

--- backend/src/main/java/com/jibangyoung/domain/mentor/service/AdMentorLogListService.java
+++ backend/src/main/java/com/jibangyoung/domain/mentor/service/AdMentorLogListService.java
@@ -1,6 +1,13 @@
 package com.jibangyoung.domain.mentor.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.jibangyoung.domain.mentor.dto.AdMentorLogListDTO;
+import com.jibangyoung.domain.mentor.repository.AdMentorLogListRepository;
+import com.jibangyoung.domain.mentor.repository.AdMentorUserRepository;
+import com.jibangyoung.domain.mypage.entity.ReviewResultCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -8,17 +15,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AdMentorLogListService {
 
-    // private final AdMentorLogListRepository adMentorLogListRepository;
-    // private final AdMentorUserRepository adMentorUserRepository;
+    private final AdMentorLogListRepository adMentorLogListRepository;
+    private final AdMentorUserRepository adMentorUserRepository;
 
-    // // 멘토 데시보드_멘토 활동로그 리스트
-    // public List<AdMentorLogListDTO> getMentorLogList(Long userId) {
-    //     List<Long> regionIds = adMentorUserRepository.findRegionIdByUserId(userId);
+    // 멘토 데시보드_멘토 활동로그 리스트
+    public List<AdMentorLogListDTO> getMentorLogList(Long userId) {
+        List<Long> regionIds = adMentorUserRepository.findRegionIdByUserId(userId);
 
-    //     if (regionIds == null || regionIds.isEmpty()) {
-    //         return List.of(); // region이 없을 경우 빈 리스트 반환
-    //     }
+        if (regionIds == null || regionIds.isEmpty()) {
+            return List.of(); // region이 없을 경우 빈 리스트 반환
+        }
 
-    //     return adMentorLogListRepository.findMentorLogListByRegionIds(regionIds);
-    // }
+        // enum 값 넘기기
+        return adMentorLogListRepository.findMentorLogListByRegionIds(regionIds, ReviewResultCode.APPROVED);
+    }
 }
