20504ff5ab5bf2031fa45959d940250e316281b5
kheun-jav <kiheun00@gmail.com>
2025-08-01 10:53:29 +0900

    동적라우팅 promise 처리

--- frontend/app/recommendation/[userId]/[responseId]/page.tsx
+++ frontend/app/recommendation/[userId]/[responseId]/page.tsx
@@ -1,12 +1,16 @@
 import RecommendationDataLoader from './RecommendationDataLoader';
 
-export default async function Page({
-  params,
-}: {
-  params: { userId: string; responseId: string };
-}) {
-  const userId = Number(params.userId);
-  const responseId = Number(params.responseId);
+interface PageProps {
+  params: Promise<{
+    userId: string;
+    responseId: string;
+  }>;
+}
+
+export default async function Page({ params }: PageProps) {
+  const  userId  = Number((await params).userId);
+  const  responseId  = Number((await params).responseId);
+
 
   if (isNaN(userId) || isNaN(responseId)) {
     return <p>잘못된 URL입니다.</p>;
