fb762d7b0ac253b16b463e3e990c324b6c03615e
WONDongin <bangsa100@naver.com>
2025-08-03 12:48:57 +0900

    신고리스트_출력완료

--- backend/src/main/java/com/jibangyoung/domain/mentor/controller/AdMentorUserController.java
+++ backend/src/main/java/com/jibangyoung/domain/mentor/controller/AdMentorUserController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jibangyoung.domain.mentor.dto.AdMentorLogListDTO;
+import com.jibangyoung.domain.mentor.dto.AdMentorReportDTO;
 import com.jibangyoung.domain.mentor.dto.AdMentorUserDTO;
 import com.jibangyoung.domain.mentor.service.AdMentorLogListService;
+import com.jibangyoung.domain.mentor.service.AdMentorReportService;
 import com.jibangyoung.domain.mentor.service.AdMentorUserService;
 import com.jibangyoung.global.security.CustomUserPrincipal;
 
@@ -22,6 +25,8 @@ public class AdMentorUserController {
 
     private final AdMentorUserService adMentorUserService;
     private final AdMentorLogListService adMentorLogListService;
+    private final AdMentorReportService adMentorReportService;
+
 
     // 멘토 데시보드_내 지역 멘토 리스트
     @GetMapping("/local")
@@ -41,5 +46,17 @@ public class AdMentorUserController {
     ) {
         Long userId = loginUser.getId();
         return adMentorLogListService.getMentorLogList(userId);
+    }
+    // 멘토가 자신의 지역 신고내역을 조회
+    @GetMapping("/report")
+    public List<AdMentorReportDTO> getMentorRegionReports(
+        @AuthenticationPrincipal CustomUserPrincipal loginUser,
+        @RequestParam("type") String type
+    ) {
+        Long mentorUserId = loginUser.getId();
+        System.out.println("[컨트롤러] mentorUserId: " + mentorUserId);
+        System.out.println("[컨트롤러] type 파라미터: " + type);
+
+        return adMentorReportService.getReportsByMentorRegionAndType(mentorUserId, type);
     }
 }
